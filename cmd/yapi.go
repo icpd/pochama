3734bee680b08c4d2b75fbb33696ab4c9d70de99
icpd <35096485+icpd@users.noreply.github.com>
2022-06-23 18:30:52 +0800

    feat: add yapi

---
+++ cmd/yapi.go
@@ -0,0 +1,87 @@
+package cmd
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const importDataPath = "/api/open/import_data"
+
+var yapiCmd = &cobra.Command{
+	Use:   "yapi",
+	Short: "yapi cli tool",
+	Run:   yapi,
+	PreRun: func(cmd *cobra.Command, args []string) {
+		viper.SetConfigName("yapi")
+	},
+}
+
+var yapiProjectName string
+var yapiUpload bool
+var yapiPrintConfig bool
+var yapiMerge string
+
+func init() {
+	yapiCmd.Flags().StringVarP(&yapiProjectName, "projectname", "n", "", "project name")
+	yapiCmd.Flags().BoolVar(&yapiUpload, "upload", false, "upload swagger file to yapi")
+	yapiCmd.Flags().BoolVar(&yapiPrintConfig, "printconfig", false, "print config")
+	yapiCmd.Flags().StringVarP(&yapiMerge, "merge", "m", "good", "sync model: normal (普通模式) , good (智能合并), merge (完全覆盖)")
+	yapiCmd.MarkFlagsRequiredTogether("upload", "projectname")
+
+	rootCmd.AddCommand(yapiCmd)
+}
+
+type YapiProjects map[string]string
+
+type YapiConfig struct {
+	Host     string       `yaml:"host" json:"host"`
+	Projects YapiProjects `yaml:"projects" json:"projects"`
+}
+
+func (c *YapiConfig) loadConfig() {
+	cobra.CheckErr(viper.ReadInConfig())
+	cobra.CheckErr(viper.Unmarshal(c))
+}
+
+func yapi(cmd *cobra.Command, args []string) {
+	var config YapiConfig
+
+	if initCfg {
+		config.Host = "http://localhost.yapi.com"
+		config.Projects = YapiProjects{
+			"project1": "example0269f91f0dab3b6624f33873f9164a2d81efaf24dcfdf1cc50d4f8f5f",
+		}
+		err := createCfgIFNotExists(config)
+		cobra.CheckErr(err)
+		return
+	}
+
+	config.loadConfig()
+
+	if yapiPrintConfig {
+		bytes, err := json.MarshalIndent(config, "", "\t")
+		cobra.CheckErr(err)
+		cmd.Print(string(bytes))
+		return
+	}
+
+	if yapiUpload {
+		file, err := ioutil.ReadFile("./docs/swagger.json")
+		cobra.CheckErr(err)
+
+		resp, err := resty.New().SetBaseURL(config.Host).R().
+			SetBody(map[string]any{
+				"type":  "swagger",
+				"json":  string(file),
+				"merge": yapiMerge,
+				"token": config.Projects[yapiProjectName],
+			}).Post(importDataPath)
+		cobra.CheckErr(err)
+
+		cmd.Println(resp.String())
+	}
+}
--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/icpd/pochama
 go 1.18
 
 require (
-	github.com/spf13/cobra v1.4.0
+	github.com/go-resty/resty/v2 v2.7.0
+	github.com/spf13/cobra v1.5.0
+	github.com/spf13/viper v1.12.0
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
+	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
@@ -20,11 +22,10 @@ require (
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.12.0 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0 // indirect
 )
