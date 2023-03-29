78a450511d16156c03f585166b7866411d765996
icpd <35096485+icpd@users.noreply.github.com>
2023-03-30 01:08:00 +0800

    feat: 日历加班统计

---
+++ cmd/calendar.go
@@ -0,0 +1,129 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/jinzhu/now"
+	"github.com/olekukonko/tablewriter"
+	"github.com/spf13/cast"
+	"github.com/spf13/cobra"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var calendarCmd = &cobra.Command{
+	Use:   "calendar",
+	Short: "print calendar events",
+	Run:   calendar,
+}
+
+var (
+	dbPath    string
+	startDate string
+)
+
+func init() {
+	homeDir, _ := os.UserHomeDir()
+	calendarCmd.Flags().StringVarP(&dbPath, "db", "", fmt.Sprintf("%s/Library/Calendars/Calendar.sqlitedb", homeDir), "SQLite数据库文件路径")
+	calendarCmd.Flags().StringVarP(&startDate, "start", "s", now.BeginningOfMonth().Format("2006-01-02"), "开始时间")
+
+	rootCmd.AddCommand(calendarCmd)
+}
+
+type CalendarItem struct {
+	Summary   string
+	StartDate string
+	EndTDate  string
+}
+
+// UTCTime20010101 978307200 = 2001-01-01 00:00:00 UTC
+const UTCTime20010101 = 978307200
+
+func calendar(cmd *cobra.Command, args []string) {
+	db, err := gorm.Open(sqlite.Open(dbPath))
+	if err != nil {
+		cobra.CheckErr(err)
+	}
+
+	startTime, err := time.Parse("2006-01-02", startDate)
+	if err != nil {
+		cobra.CheckErr(err)
+	}
+	startCalTime := startTime.Add(-UTCTime20010101 * time.Second)
+
+	var items []CalendarItem
+	err = db.Table("CalendarItem").
+		Select(fmt.Sprintf("summary, %s,  %s", toUnixTimeColumn("start_date"), toUnixTimeColumn("end_date"))).
+		Where("summary like '加班%小时'").
+		Where("start_date >= ?", startCalTime.Unix()).
+		Order("start_date").
+		Find(&items).Error
+	if err != nil {
+		cobra.CheckErr(err)
+	}
+
+	if len(items) == 0 {
+		log.Print("没有找到加班记录")
+		os.Exit(0)
+	}
+
+	var data [][]string
+	count := 0
+	for _, v := range items {
+		t, _ := time.Parse("2006-01-02 15:04:05", v.StartDate)
+
+		data = append(data, []string{
+			fmtDate(t),
+			fmtSummary(v)},
+		)
+
+		count += cast.ToInt(strings.Trim(v.Summary, "加班小时"))
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"日期", "时长"})
+	table.SetFooter([]string{fmt.Sprintf("%d天", len(items)), fmt.Sprintf("%d小时", count)}) // Add Footer
+	table.SetBorder(false)                                                                // Set Border to false
+	table.AppendBulk(data)                                                                // Add Bulk Data
+	table.Render()                                                                        // Send output
+}
+
+func toUnixTimeColumn(columnName string) string {
+	return fmt.Sprintf("datetime(%s + %d, 'unixepoch', 'localtime') as %[1]s", columnName, UTCTime20010101)
+}
+
+func fmtSummary(v CalendarItem) string {
+	return strings.Trim(v.Summary, "加班")
+}
+
+func fmtDate(t time.Time) string {
+	return fmt.Sprintf("%s %s",
+		t.Format("01-02"),
+		weekdayToChinese(t.Weekday()))
+}
+
+// weekday to chinese
+func weekdayToChinese(weekday time.Weekday) string {
+	switch weekday {
+	case time.Sunday:
+		return "日"
+	case time.Monday:
+		return "一"
+	case time.Tuesday:
+		return "二"
+	case time.Wednesday:
+		return "三"
+	case time.Thursday:
+		return "四"
+	case time.Friday:
+		return "五"
+	case time.Saturday:
+		return "六"
+	default:
+		return ""
+	}
+}
--- go.mod
+++ go.mod
@@ -1,25 +1,32 @@
 module github.com/icpd/pochama
 
-go 1.18
+go 1.20
 
 require (
 	github.com/go-resty/resty/v2 v2.7.0
+	github.com/jinzhu/now v1.1.5
+	github.com/olekukonko/tablewriter v0.0.5
+	github.com/spf13/cast v1.5.0
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/viper v1.12.0
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
 	gopkg.in/yaml.v2 v2.4.0
+	gorm.io/driver/sqlite v1.4.4
+	gorm.io/gorm v1.24.6
 )
 
 require (
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/mattn/go-sqlite3 v1.14.15 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
-	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
